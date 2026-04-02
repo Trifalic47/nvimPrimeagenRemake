d7c4fc7d6c4f46b183f7938afaca86880285b09e
Trifalic47 <trifalicapt@gmail.com>
2026-04-02 19:14:23 +0530

    Adding <leader>s and <leader>r compaitability for the visual mode

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -50,7 +50,10 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("v", "<leader>s", [["sy:%s/<C-r>s/<C-r>s/gI<Left><Left><Left>]])
+
 vim.keymap.set("n", "<leader>r", [[:s/\<<C-r><C-w>\>/]], { desc = "Replace word instance" })
+vim.keymap.set("v", "<leader>r", [["sy:s/<C-r>s/]], { desc = "Replace selection instance" })
 vim.keymap.set("n", "<leader>rw", "ciw", { desc = "Change current word" })
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
