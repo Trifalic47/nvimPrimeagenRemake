5713dfb40f5eb221a2242e05898941b55177c9ae
Tanishq (Trifalic47) <trifalicapt@gmail.com>
2026-04-14 04:06:21 +0530

    Added laygit.nvim
    Be sure to install the lazygit in your system earlier

---
+++ lua/theprimeagen/lazy/lazygit.lua
@@ -0,0 +1,11 @@
+return {
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>",                    { desc = "LazyGit" })
+        vim.keymap.set("n", "<leader>gf", "<cmd>LazyGitFilter<cr>",              { desc = "LazyGit File History" })
+        vim.keymap.set("n", "<leader>gc", "<cmd>LazyGitFilterCurrentFile<cr>",   { desc = "LazyGit Current File" })
+    end,
+}
