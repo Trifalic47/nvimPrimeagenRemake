4d38a3325fa6723da1ebe1a70b5955595a6292d6
Tanishq (Trifalic47) <trifalicapt@gmail.com>
2026-04-14 03:10:51 +0530

    errors....

--- lua/theprimeagen/lazy/treesitter.lua
+++ lua/theprimeagen/lazy/treesitter.lua
@@ -9,19 +9,15 @@ return {
 			require("nvim-treesitter.configs").setup({
 				ensure_installed = {
 					"c",
-                    "cpp",
 					"lua",
                     "rust",
 				},
 
 				sync_install = false,
-
 				auto_install = true,
-
 				indent = {
 					enable = true,
 				},
-
 				highlight = {
 					enable = true,
 					disable = function(lang, buf)
@@ -29,7 +25,6 @@ return {
 							print("disabled")
 							return true
 						end
-
 						local max_filesize = 100 * 1024 -- 100 KB
 						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 						if ok and stats and stats.size > max_filesize then
@@ -50,7 +45,6 @@ return {
 				install_info = {
 					url = "https://github.com/vrischmann/tree-sitter-templ.git",
 					files = { "src/parser.c", "src/scanner.c" },
-					branch = "master",
 				},
 			}
 
@@ -60,7 +54,7 @@ return {
 
 	{
 		"nvim-treesitter/nvim-treesitter-context",
-		after = "nvim-treesitter",
+		-- after = "nvim-treesitter",
 		config = function()
 			require("treesitter-context").setup({
 				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
