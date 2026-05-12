494efa1ac7b27302114cbe3718c7db878d63c74a
Trifalic47 <trifalicapt@gmail.com>
2026-05-12 13:47:48 +0530

    Making netrw window all details visible

--- lua/theprimeagen/init.lua
+++ lua/theprimeagen/init.lua
@@ -8,9 +8,6 @@ local ThePrimeagenGroup = augroup('ThePrimeagen', {})
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
-function R(name)
-    require("plenary.reload").reload_module(name)
-end
 
 vim.filetype.add({
     extension = {
@@ -64,9 +61,6 @@ autocmd('LspAttach', {
     end
 })
 
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
 
  -- Man Page Configuration
  vim.api.nvim_create_autocmd("FileType", {
--- lua/theprimeagen/lazy_init.lua
+++ lua/theprimeagen/lazy_init.lua
@@ -1,14 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
+  print("Installing lazy.nvim... (wait)")
   vim.fn.system({
     "git",
     "clone",
-    "--filter=blob:none",
+    "--depth=1",  -- faster, less hanging
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
