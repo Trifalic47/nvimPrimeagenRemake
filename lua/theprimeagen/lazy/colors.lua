4cce9186850728b46c2debe0177fdcb71c8f5535
Trifalic47 <trifalicapt@gmail.com>
2026-08-06 20:47:36 +0530

    Updating themee

--- lua/theprimeagen/lazy/colors.lua
+++ lua/theprimeagen/lazy/colors.lua
@@ -1,39 +1,27 @@
-function ColorMyPencils(color)
-    color = color or "rose-pine-moon"
-    vim.cmd.colorscheme(color)
+local function ColorMyPencils()
+    vim.cmd.colorscheme("gruber-darker")
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
 end
 
 return {
     {
-        "oskarnurm/koda.nvim",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            -- require("koda").setup({ transparent = true })
-            vim.cmd("colorscheme koda")
-        end,
-    },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require('rose-pine').setup({
-                disable_background = true,
-                styles = {
-                    italic = true,
-                },
-            })
-
-            ColorMyPencils();
-        end
-    },
-    {
-        "folke/tokyonight.nvim",
+        "blazkowolf/gruber-darker.nvim",
         lazy = false,
         priority = 1000,
-        opts = {},
+
+        opts = {
+            bold = false,
+            italic = {
+                strings = false,
+            },
+        },
+
+        config = function(_, opts)
+            require("gruber-darker").setup(opts)
+            ColorMyPencils()
+        end,
     },
 }
